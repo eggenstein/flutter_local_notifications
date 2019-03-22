f39e001015cba4433f11d13644c3eb6f22a424df
Mark Eggenstein <mark.eggenstein@eggnstone.com>
2019-03-22 15:41:11 +0100

    Fixed that notifications are rescheduled on Android after app update ("package replaced").

--- android/src/main/java/com/dexterous/flutterlocalnotifications/ScheduledNotificationBootReceiver.java
+++ android/src/main/java/com/dexterous/flutterlocalnotifications/ScheduledNotificationBootReceiver.java
@@ -9,9 +9,16 @@ public class ScheduledNotificationBootReceiver extends BroadcastReceiver
     @Override
     public void onReceive(final Context context, Intent intent) {
         String action = intent.getAction();
-        System.out.println("rebooted");
-        if (action != null && action.equals(android.content.Intent.ACTION_BOOT_COMPLETED)) {
-            FlutterLocalNotificationsPlugin.rescheduleNotifications(context);
+        if (action != null)
+        {
+            if (action.equals(android.content.Intent.ACTION_BOOT_COMPLETED)) {
+                System.out.println("flutter_local_notifications: boot completed => scheduling notifications");
+                FlutterLocalNotificationsPlugin.rescheduleNotifications(context);
+            }
+            else if (action.equals(android.content.Intent.ACTION_MY_PACKAGE_REPLACED)) {
+                System.out.println("flutter_local_notifications: package replaced => scheduling notifications");
+                FlutterLocalNotificationsPlugin.rescheduleNotifications(context);
+            }
         }
     }
 }
